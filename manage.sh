262d25ecfdb03603e3d06313eaeb2899611a1b5c
luisAraujo11 <laba_16_araujo@hotmail.com>
2024-06-17 21:18:48 +0100

    manage updated

--- manage.sh
+++ manage.sh
@@ -6,17 +6,17 @@ function rebuild() {
     echo "Rebuilding containers and images..."
     docker-compose down
     docker rmi -f ewproj_frontend ewproj_backend mongo:latest || true
-    docker-compose up -d --build
+    docker compose up -d --build
 }
 
 function start() {
     echo "Starting containers..."
-    docker-compose up -d --build
+    docker compose up -d --build
 }
 
 function remove() {
     echo "Stopping and removing containers, networks, and images..."
-    docker-compose down
+    docker compose down
     docker rmi -f ewproj_frontend ewproj_backend mongo:latest || true
 }
 
